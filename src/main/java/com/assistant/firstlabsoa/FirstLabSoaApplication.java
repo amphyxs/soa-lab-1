69a69b544c07cc915d166494e07b710d48cc83c5
Alexey Volkov <alexvolkovprogrammer@yandex.com>
2025-09-11 13:08:25 +0300

    First Version

---
+++ src/main/java/com/assistant/firstlabsoa/FirstLabSoaApplication.java
@@ -0,0 +1,13 @@
+package com.assistant.firstlabsoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstLabSoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstLabSoaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assistant/firstlabsoa/FirstLabSoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assistant.firstlabsoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstLabSoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
